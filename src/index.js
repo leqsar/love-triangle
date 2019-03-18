0847bd1b8a8914985a5ae28eb3b7ad672fd23101
leqsar <47753538+leqsar@users.noreply.github.com>
2019-03-18 12:32:12 +0300

    2 task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var a;
+  var b;
+  var sum = 0;
+  for (var i = 0; i < preferences.length; i++) {
+      a=preferences[i];
+      b=preferences[a-1];
+      if (preferences[b-1]==i+1) {
+        sum++;
+      }
+  }
+  return Math.floor(sum/3);
 };
